ddbbea6ca5bd6c3bd0d335cb13a1c5301ee0ca99
Stephenmuhindi <bteneson42@gmail.com>
2023-08-22 00:48:50 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,43 +1,57 @@
 #include "main.h"
+#include <stdarg.h>
+
 /**
-* _printf - output produced according to format
-* @format: character string
-* Return: characters printed
-*/
+ * _printf - displays the string to handle
+ * @format: specifiers
+ * Retuns: number of bytes or string
+ */
+
 int _printf(const char *format, ...)
 {
-	vert_same q[] = {
-		{"%s", printf_str}, {"%c", printf_char},
-		{"%%", printf_cent},
-		{"%i", printf_int}, {"%d", printf_deci}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_b}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exs_str}, {"%p", printf_pot}
-	};
-	va_list argumentz;
-	int z = 0, l, length = 0;
+	unsigned int q, counter = 0, counter_string = 0;
 
-	va_start(argumentz, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-Here:
-	while (format[z] != '\0')
+	va_list(arguments);
+
+	if
+		(!format || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
+
+	va_start(arguments, format);
+
+	for (q = 0; format[q] != '\0'; q++)
 	{
-		l = 13;
-		while (l >= 0)
+		if (format[q] != "%")
 		{
-			if (q[l].id[0] == format[z] && q[l].id[1] == format[z + 1])
-			{
-				length += q[l].f(argumentz);
-				z = z + 2;
-				goto Here;
-			}
-			l--;
+			putc(format[q]);
 		}
-		_putc(format[z]);
-		length++;
-		z++;
-	}
-	va_end(argumentz);
-	return (length);
+		else if (format[q] == '%' && format[q + 1] ==  'c')
+		{
+			putc(va_arg(arguments, int);
+					q++;
+		}
+		else if (format[q] == '%' && format[q + 1] ==  'd')
+		{
+		putc(va_arg(arguments, int);
+				q++;
+		}
+		else if (format[q] == '%' && format[q + 1] == 's')
+		{
+		counter_string = (va_args(arguments, char *);
+				counter += (counter_string - 1);
+		}
+		else if (format[q] == '%' && format[q + 1] == '%')
+		{
+		putc('%');
+		}
+		else if (format[q] == '%' && format[q + 1] ==  'i')
+		{
+		putc(va_arg(arguments, int);
+		q++;
+		}
+		counter++;
+
+		va_end(arguments);
+
+		return (counter);
 }
