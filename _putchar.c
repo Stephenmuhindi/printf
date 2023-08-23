b4a3c8a0e23a6cf05c6a55f1c2b5a6184169af8d
Kabila Kasongo <kabila.edith@gmail.com>
2023-08-23 02:18:05 -0700

    hfdd

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
-#include "hols.h"
+#include "main.h"
 
-/**
- * write_char - displays c to stdout
- * @c: char
- * Return: 1
- */
 #include <unistd.h>
-/*#include <stdio.h>*/
+
 
 /**
  * _putchar - displays c to stdout
@@ -15,7 +10,7 @@
  * Return: On success 1. alternatively -1
  */
 
-int write_char(char c)/*int _putchar(char c)*/
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
