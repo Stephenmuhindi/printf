3828438e668cb619b8d6220a3654548bc200acc3
Kabila Kasongo <kabila.edith@gmail.com>
2023-08-23 02:14:33 -0700

    gfd

--- _putchar.c
+++ _putchar.c
@@ -1,25 +1,21 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include "hols.h"
 
 /**
  * write_char - displays c to stdout
-=======
+ * @c: char
+ * Return: 1
+ */
 #include <unistd.h>
-#include <stdio.h>
+/*#include <stdio.h>*/
 
 /**
  * _putchar - displays c to stdout
->>>>>>> 19bf3b09ef125fb62bfd1a850e89f40c3f0085bf
  * @c: input
  * Return: On success 1. alternatively -1
  */
-<<<<<<< HEAD
-int write_char(char c)
-=======
 
-int _putchar(char c)
->>>>>>> 19bf3b09ef125fb62bfd1a850e89f40c3f0085bf
+int write_char(char c)/*int _putchar(char c)*/
 {
 	return (write(1, &c, 1));
 }
