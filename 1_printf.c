2da3ae1aedd566615a19b82e2a2eafbe062c51ce
Kabila Kasongo <kabila.edith@gmail.com>
2023-08-24 02:22:15 -0700

    hgfd

--- 1_printf.c
+++ 1_printf.c
@@ -16,16 +16,6 @@ int _printf(const char *format, ...)
 	va_list list;
 
 	char buffer[BUFF_SIZE];
-/**
- * _printf - Produces output according to a format.
- * @format: The format string.
- * Return: amount of bytes
- */
-int _printf(const char *format, ...)
-{
-	int q = 0;
-
-	va_list(args);
 
 	if (format == NULL)
 		return (-1);
@@ -75,30 +65,5 @@ void print_buffer(char buffer[], int *buff_ind)
 		write(1, &buffer[0], *buff_ind);
 	}
 
-	*buff_ind = 
-	va_start(args, format);
-
-	while (*format != '\0')
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			q++;
-			format++;
-		} else
-		{
-			format++;
-			if (*format == 'd' || *format == 'i')
-			{
-				int num = va_arg(args, int);
-				char my_str[20];
-
-				sprintf(my_str, "%d", num);
-				q += write(1, my_str, strlen(my_str));
-			}
-			format++;
-		}
-	}
-	va_end(args);
-	return (q);
+	*buff_ind = 0;
 }
--- main.h
+++ main.h
@@ -93,5 +93,7 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 int _printf(const char *format, ...);
+int _puts(char *c);
+int _putchar(char c);
 
 #endif/*end of main.h*/
