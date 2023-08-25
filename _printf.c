f9ee241e31fcb0773dc69025724628ebef6b1fd4
Stephenmuhindi <bteneton42@gmail.com>
2023-08-25 00:43:08 -0700

    mwana

--- _printf.c
+++ _printf.c
@@ -7,46 +7,56 @@
 * @format: specifiers
 * Return: amount of bytes
 */
+
 int _printf(const char *format, ...)
 {
-	unsigned int q;
-	int count = 0;
-	va_list args;
+    unsigned int q;
+    int count = 0;
+    va_list args;
 
-	if (!format || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-	va_start(args, format);
-	for (q = 0; format[q] != '\0'; q++)
-	{
-		if (format[q] != '%')
-		{
-			_putchar(format[q]); /* Use putchar to output a single character */
-		} else if (format[q + 1] == 'c')
-		{
-			_putchar(va_arg(args, int));
-			q++;
-		} else if (format[q + 1] == 's')
-		{
-			char *str = va_arg(args, char *);
+    if (!format || (format[0] == '%'&& format[1] == '\0'))
+        return (-1);
+    va_start(args, format);
+    for (q = 0; format[q] != '\0'; q++)
+    {
+        if (format[q] != '%')
+        {
+            _putchar(format[q]); /* Use putchar to output a single character */
+        } 
+        else if (format[q + 1] == 'c')
+        {
+            _putchar(va_arg(args, int));
+            q++;
+            count++;
+        } 
+        else if (format[q + 1] == 's')
+        {
+            char *str = va_arg(args, char *);
+            while (*str)
+            {
+                _putchar(*str);
+                str++;
+                count++;
+            }
+            q++;
+        } 
+        else if (format[q + 1] == 'd' || format[q + 1] == 'i')
+        {
+            int num = va_arg(args, int);
+            char my_str[20];
 
-			_puts(str); /* Use puts to output a string */
-			count += strlen(str);
-			q++;
-		} else if (format[q + 1] == 'd' || format[q + 1] == 'i')
-		{
-			int num = va_arg(args, int);
-			char my_str[20];
-
-			sprintf(my_str, "%d", num);
-			count += write(1, my_str, strlen(my_str));
-			q++;
-		} else if (format[q + 1] == '%')
-		{
-			_putchar('%');
-			q++;
-		}
-		count++;
-	}
-	va_end(args);
-	return (count);
+            sprintf(my_str, "%d", num);
+            count += write(1, my_str, strlen(my_str));
+            q++;
+        } 
+        else if (format[q + 1] == '%')
+        {
+            _putchar('%');
+            q++;
+            count++;
+        }
+        count++;
+    }
+    va_end(args);
+    return count;
 }
