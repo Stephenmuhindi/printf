48e909ddfc97b8bb2322df166d474c4f564bdfef
Stephenmuhindi <bteneson42@gmail.com>
2023-08-27 12:42:08 -0700

    wueh

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>/* used by variadic function*/
+#include <unistd.h>/* used by putchar*/
+#include <limits.h>
+#include <string.h>
+
+int _printf(const char *format, ...);
+int _putc(char c);
+int putzz(char *c);
+
+#endif/*end of thei main.h */
