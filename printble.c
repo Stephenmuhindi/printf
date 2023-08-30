f417be403524ff30de1a8ed0401d6773ef82ae81
Stephenmuhindi <bteneson42@gmail.com>
2023-08-30 03:35:07 -0700

    finally

---
+++ printble.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+/**
+ * is_printable - checks if char is printable
+ * @c: Char
+ * Return: 1 else 0
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hexa_code - ongeza ascci in hexadecimal code
+ * @buffer: Array char.
+ * @i: Index to start.
+ * @ascii_code: ASSCI.
+ * Return: Always 3
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
+/**
+ * is_digit - checks if char is num
+ * @c: Char
+ * Return: 1 else 0
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * convert_size_number - Casts int to specific size
+ * @num: Num.
+ * @size: Num indicating type
+ * Return: Casted value of num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - Casts num to certain size
+ * @num: Num
+ * @size: Num indicating type
+ * Return: value of num
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
